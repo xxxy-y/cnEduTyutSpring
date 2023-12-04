980636d6e27816168a20e2c556b73c3c0d29861d
羊羊 <wqt1922311386@outlook.com>
2023-12-04 21:22:17 +0800

    20:23

--- integrationOfSpringAndMybatisAnnotation/src/test/java/MainTest.java
+++ integrationOfSpringAndMybatisAnnotation/src/test/java/MainTest.java
@@ -16,6 +16,13 @@ import javax.swing.*;
  * @Time 20:14
  * @Package_Name PACKAGE_NAME
  */
+
+/**
+ * 当一个测试类使用@ExtendWith(SpringExtension.class)注解时，JUnit在运行该测试类时会使用SpringExtension作为运行器
+ * 这将启动Spring容器，并在测试期间加载和管理Bean，以及提供Spring的其他特性，如依赖注入、事务管理等。
+ * 因此，通过@ExtendWith(SpringExtension.class)，测试类可以在Spring环境下运行，
+ * 可以使用Spring提供的功能和特性进行测试，从而更好地测试Spring应用程序的各个组件的行为和交互。
+ */
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = MainConfiguration.class)
 public class MainTest {
